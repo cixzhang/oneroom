997a43e0eb393cace763c0524c94ab7a1cf4da55
Cindy Zhang <cixzhang@gmail.com>
2018-02-12 21:58:25 -0800

    adding basic movement

--- main-state.js
+++ main-state.js
@@ -3,24 +3,47 @@
 
 var mainState = {
     preload: function() {
-        // Here we preload the assets
+      // Here we preload the assets
+      game.load.image('player', 'assets/sprites/base.png');
 
-        // game scaling
-        game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
-        game.scale.setUserScale(3, 3);
+      // game scaling
+      game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+      game.scale.setUserScale(3, 3);
 
-        game.renderer.renderSession.roundPixels = true;
-        Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
+      game.renderer.renderSession.roundPixels = true;
+      Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
     },
 
     create: function() {
-        // Here we create the game
+      // Here we create the game
+      game.stage.backgroundColor = '#3598db';
 
-        // Start the Arcade physics system (for movements and collisions)
-        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+      // Start the Arcade physics system (for movements and collisions)
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+      game.world.enableBody = true;
+
+      this.cursor = game.input.keyboard.createCursorKeys();
+      this.player = game.add.sprite(70, 100, 'player');
+
     },
 
     update: function() {
-        // Here we update the game 60 times per second
+      // Here we update the game 60 times per second
+      if (this.cursor.left.isDown)
+        this.player.body.velocity.x = -200;
+      else if (this.cursor.right.isDown)
+        this.player.body.velocity.x = 200;
+      else
+        this.player.body.velocity.x = 0;
+
+
+      if (this.cursor.up.isDown)
+        this.player.body.velocity.y = -200;
+      else if (this.cursor.down.isDown)
+        this.player.body.velocity.y = 200;
+      else
+        this.player.body.velocity.y = 0;
     },
 };
+
